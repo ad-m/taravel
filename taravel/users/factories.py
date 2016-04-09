2b2d58a62412317c1187461ac713bfe163ccb400
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-04-09 15:31:34 +0200

    Add django-extra-views==0.7.1

--- taravel/users/factories.py
+++ taravel/users/factories.py
@@ -8,15 +8,3 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = models.User
-
-
-class AddressFactory(factory.django.DjangoModelFactory):
-    name = factory.Sequence(lambda n: 'name-%04d' % n)
-    street = factory.Sequence(lambda n: 'street-%04d' % n)
-    street_number = factory.Sequence(lambda n: '%da' % n)
-    postcode = factory.Sequence(lambda n: '00-%03d' % n)
-    city = factory.Sequence(lambda n: 'city-%04d' % n)
-    taxpayer_id = factory.Sequence(lambda n: n)
-
-    class Meta:
-        model = models.Address

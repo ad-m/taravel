322c8cb3a21c45b1ad4d155750f778d84341f0e1
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-04-09 02:41:46 +0200

    Fix Guest model name, non-child default, test for model Guest and factories

---
+++ taravel/guests/factories.py
@@ -0,0 +1,16 @@
+import factory
+import factory.fuzzy
+from . import models
+
+from taravel.orders.factories import OrderFactory
+
+
+class GuestFactory(factory.django.DjangoModelFactory):
+    order = factory.SubFactory(OrderFactory)
+    first_name = factory.Sequence(lambda n: 'first_name-%04d' % n)
+    second_name = factory.Sequence(lambda n: 'first_name-%04d' % n)
+    government_id = factory.fuzzy.FuzzyInteger(low=100, high=200)
+    value = factory.LazyAttribute(lambda x: x.order.trip.base_price)
+
+    class Meta:
+        model = models.Guest
--- taravel/guests/models.py
+++ taravel/guests/models.py
@@ -9,23 +9,25 @@ from model_utils.models import TimeStampedModel
 from taravel.orders.models import Order
 
 
-class QuestQuerySet(models.QuerySet):
+class GuestQuerySet(models.QuerySet):
     pass
 
 
 @python_2_unicode_compatible
-class Quest(TimeStampedModel):
+class Guest(TimeStampedModel):
     order = models.ForeignKey(to=Order, verbose_name=_("Order"))
     first_name = models.CharField(verbose_name=_("First name"), max_length=50)
     second_name = models.CharField(verbose_name=_("Second name"), max_length=50)
     government_id = models.IntegerField(verbose_name=_("Government ID"))
-    child = models.BooleanField(verbose_name=_("Child"), help_text=_("Is a child?"))
+    child = models.BooleanField(verbose_name=_("Child"),
+                                help_text=_("Is a child?"),
+                                default=False)
     value = models.IntegerField(verbose_name=_("Value"))  # TODO: Custom model field
-    objects = QuestQuerySet.as_manager()
+    objects = GuestQuerySet.as_manager()
 
     class Meta:
-        verbose_name = _("Quest")
-        verbose_name_plural = _("Quests")
+        verbose_name = _("Guest")
+        verbose_name_plural = _("Guests")
 
     def __str__(self):
         return "{first_name} {second_name}".format(first_name=self.first_name,

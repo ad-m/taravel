562cd7db3ba229f672fbf2747bbce7e3054b9f26
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-04-09 17:06:52 +0200

    Use Inlines to manipulate Images in trips

--- taravel/trips/views.py
+++ taravel/trips/views.py
@@ -1,3 +1,4 @@
+from atom.ext.crispy_forms.forms import BaseTableFormSet
 from atom.views import DeleteMessageMixin
 from braces.views import FormValidMessageMixin, SelectRelatedMixin, UserFormKwargsMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
@@ -5,10 +6,11 @@ from django.core.urlresolvers import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
 from django_filters.views import FilterView
+from extra_views import CreateWithInlinesView, InlineFormSet, NamedFormsetsMixin, UpdateWithInlinesView
 
 from .filters import TripFilter
 from .forms import TripForm
-from .models import Trip
+from .models import Trip, Image
 
 
 class TripListView(FilterView):
@@ -31,19 +33,27 @@ class TripDetailView(SelectRelatedMixin, DetailView):
         return super(TripDetailView, self).dispatch(request, *args, **kwargs)
 
 
-class TripCreateView(PermissionRequiredMixin, UserFormKwargsMixin, CreateView):
+class ImageInline(InlineFormSet):  # TODO: Calculate value
+    model = Image
+    formset_class = BaseTableFormSet
+
+
+class TripCreateView(NamedFormsetsMixin, UserFormKwargsMixin, CreateWithInlinesView):
     model = Trip
     form_class = TripForm
-    permission_required = 'trips.add_trip'
+    inlines = [ImageInline]
+    inlines_names = ['images']
 
-    def get_form_valid_message(self):
-        return _("{0} created!").format(self.object)
+    def get_success_url(self):
+        return self.object.get_absolute_url()
 
 
-class TripUpdateView(PermissionRequiredMixin, FormValidMessageMixin, UpdateView):
+class TripUpdateView(NamedFormsetsMixin, FormValidMessageMixin, UpdateWithInlinesView):
     model = Trip
     form_class = TripForm
-    permission_required = 'trips.change_trip'
+    permission_required = 'orders.change_order'
+    inlines = [ImageInline]
+    inlines_names = ['images']
 
     def get_form_valid_message(self):
         return _("{0} updated!").format(self.object)
